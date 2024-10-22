7f148c36af84e30e7874dc0a712143b330dca8be
frankburgs <franciscoburgers@gmail.com>
2024-10-22 12:27:28 -0700

    Added icons and data file

---
+++ data.js
@@ -0,0 +1,32 @@
+const eduData = [
+    {
+        institution : "University of California, San Diego",
+        award : "B.S., Bioengineering",
+        icon : "../resume/ucsd.svg",
+        date : "August 2014"
+    },
+    {
+        institution : "Learn Academy",
+        award : "640-hour Full-stack Web Development Bootcamp",
+        icon : "../resume/learn.svg",
+        date : "March 2023"
+    },
+    {
+        institution : "UCSD Extension, Safety Certificates",
+        award : "General Industry, Maritime, Safety & Health",
+        icon : "../resume/uc.png",
+        date : "December 2023"
+    },
+    {
+        institution : "San Diego College of Continuing Education",
+        award : "Shielded Metal Arc Welding 1",
+        icon : "../resume/sdcce.svg",
+        date : "May 2024"
+    },
+    {
+        institution : "SkillsBuild, IBM Learning Portal Certificates",
+        award : "Introductory Information Technology & Cybersecurity",
+        icon : "../resume/skillsbuild.svg",
+        date : "In-progress"
+    },
+]
